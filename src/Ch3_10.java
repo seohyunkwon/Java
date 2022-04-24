e06fd7c3a069cd4660b890ad0bbb1addd7f38bf4
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-04-24 16:39:28 +0900

    수정

--- src/Ch3_10.java
+++ src/Ch3_10.java
@@ -5,7 +5,9 @@ public class Ch3_10 {
 		
 //		System.out.println('2'-'0'); // 2
 //		'2'-> char , '0'-> char
-		System.out.println("문자를 입력하세요.");
+		
+		System.out.println("산술변환 이해해보기");
+		System.out.println("1~9 사이의 문자를 입력하세요.");
 		
 		Scanner scanner = new Scanner(System.in);
 		char c1 = scanner.next().charAt(0); // nextChar 변수 불가, String으로 입력 후 변환해야 함
@@ -16,7 +18,7 @@ public class Ch3_10 {
 		int d1 = (int)c1;
 		int d2 = (int)c2;
 		System.out.println("그러나 int 타입으로 출력하면 유니코드인 "+d1+"로 출력된다.");
-		System.out.println("따라서, System.out,println('"+d1+"'-'0');의 값은 char 타입으로 자동 형변환 되어서 "+(d1-d2)+"의 값이 산출된다");
+		System.out.println("따라서, [System.out,println('"+d1+"'-'0');의 값은] char 타입으로 자동 형변환 되어서 "+(d1-d2)+"의 값이 산출된다");
 		
 	}
 	
