5a229403f6dfa6ffd56f3d6497bf0278814ebc66
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-04-24 16:41:51 +0900

    수정

--- src/Ch3_10.java
+++ src/Ch3_10.java
@@ -18,7 +18,7 @@ public class Ch3_10 {
 		int d1 = (int)c1;
 		int d2 = (int)c2;
 		System.out.println("그러나 int 타입으로 출력하면 유니코드인 "+d1+"로 출력된다.");
-		System.out.println("따라서, [System.out,println('"+d1+"'-'0');의 값은] char 타입으로 자동 형변환 되어서 "+(d1-d2)+"의 값이 산출된다");
+		System.out.println("따라서, [ System.out,println('"+d1+"'-'0'); ]의 값은 char 타입으로 자동 형변환 되어서 "+(d1-d2)+"의 값이 산출된다");
 		
 	}
 	
