a2b510fdd080e7df4ea2e976e53e20d02c50a649
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-06-14 21:24:45 +0900

    알쏭달쏭 자바 0614

---
+++ studyjava200/Ex05_2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex05_2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("정수 :");
+		int a = scanner.nextInt();
+		System.out.printf("8진수 : %o\n", a);
+		System.out.printf("16진수 : %x", a);
+	
+	
+	}
+}
