641ff85f1e04a1399d7df820aa40a91d966e45e2
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-06-16 00:37:23 +0900

    백준 16394번 0616

---
+++ backjoon/Ex16394.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex16394 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int year = scanner.nextInt();
+		System.out.println(year-1946);
+		
+		
+		
+	}
+}
