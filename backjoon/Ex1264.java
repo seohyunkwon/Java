9115e2637e6ab682d7fd49b7a142d36d9836339a
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-06-17 10:27:23 +0900

    백준1264 0617

---
+++ backjoon/Ex1264.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex1264 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String text = scanner.next();
+		
+		int num = 0;
+		
+		
+		
+		
+	}
+}
