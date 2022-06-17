ef78c3343c886875082310a02847fd7148908619
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-06-18 02:55:00 +0900

    백준 9316 0618

---
+++ backjoon/Ex9316.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Ex9316 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int num = scanner.nextInt();
+		
+		for(int i=1; i<=num; i++) {
+			System.out.println("Hello World, Judge "+i+"!");
+		}
+		
+	}
+}
