2642b8f819a1210c01452e74f84eae6342f0fb46
seohyunkwon <104131284+seohyunkwon@users.noreply.github.com>
2022-06-27 21:06:53 +0900

    백준 15680 0627

---
+++ backjoon/Ex15680.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ex15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n==0) System.out.println("YONSEI");
+		else if(n==1) System.out.println("Leading the Way to the Future");
+	}
+}
